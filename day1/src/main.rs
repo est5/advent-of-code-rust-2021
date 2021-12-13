6d5b75c0dd048fe7dfad4103a1b21f810b9c7071
EST <ghostrekon5500@ya.ru>
2021-12-13 23:48:45 +0300

    part 1 and part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,43 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::ops::Index;
+
 fn main() {
-    println!("Hello, world!");
+    part_one();
+    part_two();
+}
+
+fn part_one() {
+    let mut count = 0;
+    let content = File::open("day1.txt").expect("file wasn't found");
+    let reader = BufReader::new(content);
+    let numbers: Vec<isize> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<isize>().unwrap())
+        .collect();
+    let mut first = 100;
+    for number in numbers {
+        if number > first { count += 1; }
+        first = number;
+    }
+    println!("{}", count);
+}
+
+fn part_two() {
+    let mut count = 0;
+    let content = File::open("day1.txt").expect("file wasn't found");
+    let reader = BufReader::new(content);
+    let numbers: Vec<isize> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<isize>().unwrap())
+        .collect();
+    let mut prev_idx_sum = numbers.index(0) + numbers.index(1) + numbers.index(2);
+    let mut sum: isize = 0;
+    for i in 1..numbers.len() - 2 {
+        sum = numbers.index(i) + numbers.index(i + 1) + numbers.index(i + 2);
+        if sum > prev_idx_sum { count += 1; }
+        prev_idx_sum = sum;
+    }
+    println!("{}", count);
 }
