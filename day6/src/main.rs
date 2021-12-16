c0565f483d50e2a1dcc56d44acf0cbb13d3039f9
EST <ghostrekon5500@ya.ru>
2021-12-16 11:41:14 +0300

    day6 part two

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 struct Fish {
-    state: i32,
+    state: usize,
 }
 
 impl Fish {
@@ -22,27 +22,27 @@ impl Fish {
     }
 }
 
-fn fish(numbers: Vec<i32>) -> Vec<Fish> {
+fn fish(numbers: Vec<u64>) -> Vec<Fish> {
     let mut fishes: Vec<Fish> = Vec::new();
     for number in numbers {
         fishes.push(
             Fish {
-                state: number
+                state: number as usize
             }
         )
     }
     fishes
 }
 
-fn get_numbers() -> Vec<i32> {
+fn get_numbers() -> Vec<u64> {
     let data = fs::read_to_string("day6.txt").expect("wrong file");
-    let data: Vec<i32> = data.trim().split(",").map(|x| x.parse().expect("numbers")).collect();
+    let data: Vec<u64> = data.trim().split(",").map(|x| x.parse().expect("numbers")).collect();
     data
 }
 
-fn part_one() {
+fn stupid_part_one() {
     let mut fishes = fish(get_numbers());
-    let days = 80;
+    let days = 256;
     for _day in 0..days {
         for fish in 0..fishes.len() {
             let create_new = fishes[fish].day_passed();
@@ -54,6 +54,27 @@ fn part_one() {
     println!("{}", fishes.len());
 }
 
+fn clever_part_one_and_two(days: usize) {
+    let mut array:[usize;9] = fs::read_to_string("day6.txt").expect("wrong file")// 9 cuz 0 day
+        // counts too
+        .split(",")
+        .fold([0; 9], |mut array, fish| { //add each fish to it day
+            array[fish.parse::<usize>().unwrap()] += 1; // number of fishes per day
+            // ex 0 fishes for 7th day, 110 fishes for 1st day etc
+            array
+        });
+
+    for day in 1..days {
+        array[(day + 7) % 9] += array[day % 9]; // 7 number of days to spawn new fish ( 0 incl)
+        // %9 - day to put fish (idx)
+        //day 1 , on 8 day += 1st day
+        //day 2 , on 0 day += 7day
+        //day 3 , on 1 += as on 6 etc... 1 fish == 1 fish so just sum each day
+    }
+    println!("{:?}", array); // num of fishes per day
+    println!("{:?}", array.iter().sum::<usize>()); // sum of all fishes per day
+}
+
 fn main() {
-    part_one();
+    clever_part_one_and_two(256);
 }
